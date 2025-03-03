20ef6cd935dc17baf66ebec49a648fa312bbf72b
ddalynng <d.fangloy@gmail.com>
2025-03-03 19:54:30 +0100

    layout header

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,23 +12,34 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>
-        {/* header*/}
-        {/*footer*/}
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <head>
+          <link rel="icon" href="/logo.png" sizes="any" />
+        </head>
+        <body className={`${inter.className}`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Header />
+            <main className="min-h-screen">{children}</main>
+            <Toaster richColors />
 
-        <footer className=" bg-blue-500"></footer>
-        {/*sidebar*/}
-
-        <footer>
-          <div>
-            {/*TO DO*/}
-            <p> Made with love by XXX </p>
-          </div>
-        </footer>
-        {/*main*/}
-        {children}
-      </body>
-    </html>
+            <footer className="bg-muted/50 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-200">
+                <p>Made with 💗 by ddalynn</p>
+              </div>
+            </footer>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
